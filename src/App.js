24a2daec781ba3dacf97c2f305e70b441b203293
Konyaeva Olga <oly3622@yandex.ru>
2022-11-24 18:58:43 +0300

    Функци я добавление в корзину товара

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Drawer from './components/Drawer';
 //функциия открытие корзины
 function App() {
   const [items, setItems] = React.useState([])
+  const [cartItems, setCartItems] = React.useState([])
   const [cartOpened, setCartOpened] = React.useState(false);
 
   //бэкенд карточки товара залит сюда https://mockapi.io/projects/637f70212f8f56e28e8c10fc 
@@ -22,9 +23,15 @@ React.useEffect(() => {
   });
 }, []);
 
+//добавление товара в корзину 
+const onAddToCart = (obj) => {
+  setCartItems(prev => [...prev, obj]);
+};
+
+
   return (
   <div className="wrapper clear">
-    {cartOpened && <Drawer  onClose={() => setCartOpened(false) } /> } 
+    {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false) } /> } 
     <Header onClickCart={() => setCartOpened(true)}/>  {/* Шапка сайта */}
     {/* Поиск */}
     <div className="content p-40">
@@ -41,13 +48,13 @@ React.useEffect(() => {
         {/* 2 шаг указываем, где этот файл должен находится  */}
         {/* Делаем объект, чтобы передавать информацию, не дублируя ее */}
      
-        {items.map((obj) => (
+        {items.map((item) => (
           <Card
-            title= {obj.title}
-            price= {obj.price} 
-            imageUrl = {obj.imageUrl}
+            title= {item.title}
+            price= {item.price} 
+            imageUrl = {item.imageUrl}
             onFavorute = {() => console.log('Добавили в закладки')}
-            onPlus={() => console.log('Добавили в корзину')}
+            onPlus={(obj) => onAddToCart(item)}
           /> 
         ))}
        
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,24 +4,25 @@ import styles from './Card.module.sass';
 //3 шаг Создаем функцию, которая показывает часть кода
 // Карточка товара
 //props передают в объект данные страницы
-function Card(props) {
+function Card({onFavorute, onPlus, title, imageUrl, price}) {
   const [isAdded, setIsAdded ] = React.useState(false);
 
   const onClickPlus = () => {
+    onPlus( title, imageUrl, price);
     setIsAdded(!isAdded);  //Добавить в корзину и убрать с корзины
   };
 
     return (
         <div className={styles.card}>
-        <div className={styles.favorite} onClick={props.onFavorute}>
+        <div className={styles.favorite} onClick={onFavorute}>
         <img src="img/heart.svg" alt="unliked"/>
         </div>
-        <img width={133} height={112} src={props.imageUrl} alt="sneakers" />
-        <h5>{props.title}</h5>
+        <img width={133} height={112} src={imageUrl} alt="sneakers" />
+        <h5>{title}</h5>
         <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
           <span>Цена: </span>
-          <b>{props.price} руб. </b>
+          <b>{price} руб. </b>
           </div>
             <img className={styles.plus}
             // выделяем товар для корзины
